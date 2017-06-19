dbc0a55e288f11f5caf0f3e780adf4f76bc47e50
Ben Roberts <ben@thatmustbe.me>
2017-06-19 16:24:19 -0400

    taking some notes

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// prompt for login
+// verify login
+// redirect back to source
+
+// looks for me, redirect_uri, response_type, state, client_id
+                    //'me' => $me,
+                    //'redirect_uri' => $redir_url,
+                    //'response_type' => 'id',
+                    //'state' => substr(md5($trimmed_me . config('app.url') ), 0, 8),
+                    //'client_id' => config('app.url')
