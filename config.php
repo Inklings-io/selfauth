b9e4008fc490c08f3739a29370e08e955d41322d
Ben Roberts <ben@thatmustbe.me>
2017-06-23 00:06:29 -0400

    work on setup script
    refs issue #6 and issue #3
    file writing is not working for me, but i suspect it is the server i
    tried on.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_URL', '');
-define('APP_KEY', '');
-define('USER_HASH', '');
-define('USER_URL', '');
+define('APP_URL', 'http://students.cs.uri.edu/~ben/selfauth/');
+define('APP_KEY', '2c2f87d0d894481ab56feea402cfb565');
+define('USER_HASH', '2c74e9031b927396ca24b1d7a8ed86b1');
+define('USER_URL', 'http://students.cs.uri.edu/~ben');
--- setup.php
+++ setup.php
@@ -2,25 +2,81 @@
 <body>
 <h1>Setup Selfauth</h1>
 <div>In order to configure selfauth, you need to fill in a few values, this page helps generate those options.</div>
-<?php if(isset($_POST['submit'])):?>
+<?php if(isset($_POST['username'])):?>
 <div>
-Fill in the file config.php with the following content
-<pre>
 <?php
+    define('RANDOM_BYTE_COUNT', 30);
+
     $app_url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] 
       . str_replace('setup.php', '', $_SERVER['REQUEST_URI']);
-    $app_key = md5(time().$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+
+    if(function_exists('random_bytes')) {
+        $app_key = md5(random_bytes(RANDOM_BYTE_COUNT));
+    } elseif(function_exists('openssl_random_pseudo_bytes')){
+        $app_key = md5(openssl_random_pseudo_bytes(RANDOM_BYTE_COUNT));
+    } else {
+        $app_key = md5(mt_rand());
+    }
+
     $user = $_POST['username'];
 
     $user_tmp = trim(preg_replace('/^https?:\/\//', '', $_POST['username']), '/');
     $pass = md5($user_tmp . $_POST['password'] . $app_key);
+
+    $config_file_contents = "<?php
+define('APP_URL', '$app_url');
+define('APP_KEY', '$app_key');
+define('USER_HASH', '$pass');
+define('USER_URL', '$user');";
+
+
+
+    $configfile= __DIR__ . '/config.php';
+
+    $configured = true;
+
+    if(file_exists($configfile)){
+
+        require_once $configfile;
+        
+        if((!defined('APP_URL') || APP_URL == '')
+            || (!defined('APP_KEY') || APP_KEY == '')
+            || (!defined('USER_HASH') || USER_HASH == '')
+            || (!defined('USER_URL') || USER_URL == '')
+        ) {
+            $configured = false;
+        }
+    } else {
+        $configured = false;
+    }
+
+	$file_written = false;
+
+    if(is_writeable($configfile) && !$configured()){
+
+		$handle = fopen($configfile, 'w');
+
+		if($handle){
+			$result = fwrite($handle, $config_file_contents);
+			if($result !== FALSE){
+				$file_written = true;
+			}
+			
+		}
+
+		fclose($handle);
+    }
+
+
+    if($file_written){
+		echo 'config.php was successfully written to disk';
+	} else {
+        echo 'Fill in the file config.php with the following content';
+        echo '<pre>';
+        echo htmlentities($config_file_contents);
+        echo '</pre>';
+    }
  ?>
-&lt;?php 
-define('APP_URL', '<?php echo $app_url ?>');
-define('APP_KEY', '<?php echo $app_key ?>');
-define('USER_HASH', '<?php echo $pass?>');
-define('USER_URL', '<?php echo $user?>');
-</pre>
 </div>
 <?php endif ?>
 <form method="POST" action="">
@@ -28,7 +84,7 @@ define('USER_URL', '<?php echo $user?>');
 <input name='username' />
 <br>
 <label>Password</label>
-<input name='password' />
+<input type='password' name='password' />
 
 <input type="submit" name="submit" />
 </form>
